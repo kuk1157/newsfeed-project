80220e46bf611c00b50b00c6c5adf6196c94fb40
kuk1157 <kuk1938@naver.com>
2024-10-18 21:16:16 +0900

    git init

---
+++ src/main/java/com/sparta/iinewsfeedproject/IiNewsfeedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.iinewsfeedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiNewsfeedProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IiNewsfeedProjectApplication.class, args);
+    }
+
+}
